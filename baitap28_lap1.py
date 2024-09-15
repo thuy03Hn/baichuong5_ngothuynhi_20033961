17c5d91fa1b459cb2a00f733589b00955c99f47b
thuy03Hn <133358178+thuy03Hn@users.noreply.github.com>
2024-09-15 13:37:15 +0700

    Add files via upload

--- baitap28_lap1.py
+++ baitap28_lap1.py
@@ -4,17 +4,12 @@ Created on Sun Sep 15 02:15:16 2024
 
 @author: DELL _ PC
 """
-while True:
-    try:
-        n = int(input("Nhập số nguyên dương N: "))
-        if n > 0:
-            break
-        else:
-            print("N phải là số nguyên dương. Vui lòng nhập lại.")
-# In tất cả các ước số của N
-i = 1
-print("Các ước số của", n, "là:", end=" ")
-while i <= n:
-    if n % i == 0:
+N = int(input("Nhập số nguyên dương N: "))
+while N <= 0:
+    print("Vui lòng nhập số nguyên dương!")
+    N = int(input("Nhập lại số nguyên dương N: "))
+
+print("Các ước số của", N, "là:")
+for i in range(1, N + 1):
+    if N % i == 0:
         print(i, end=" ")
-    i += 1
